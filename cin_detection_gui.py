b48ed61ae75d5efe855cb6ccaf03da6bb898170e
Hamza Boulahia <hammzaboulahia@gmail.com>
2022-05-10 08:35:18 +0100

    Update cin_detection_gui.py

--- cin_detection_gui.py
+++ cin_detection_gui.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'test.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.2
-#
-# WARNING! All changes made in this file will be lost!
-
 import os
 import gc
 from operator import contains
